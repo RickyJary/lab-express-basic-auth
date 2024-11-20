2597172545f3fc6293ba57bd4ce959b4e2c039c0
RickyJary <ricardogarciajarillo0209@gmail.com>
2024-11-20 17:55:33 +0100

    starting iterations

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,12 @@
+const { signUp } = require("../controllers/auth.controller");
+
 const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get('/signup', signUp)
 
 module.exports = router;
